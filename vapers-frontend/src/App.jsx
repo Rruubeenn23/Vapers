69880e9499a1c212a11ae4ecdf6f33faf11b905f
Rruubeenn23 <rubencereceda23@gmail.com>
2025-08-08 20:10:41 +0200

    WIPE FINANZAS

--- vapers-frontend/src/App.jsx
+++ vapers-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Comprar from './pages/Comprar';
 import Vender from './pages/Vender';
-import Estadisticas from './pages/Estadisticas';
 import NuevoVaper from './pages/NuevoVaper';
 import Finanzas from './pages/Finanzas';
 
@@ -13,7 +12,6 @@ function App() {
           <Link to="/comprar" style={{ marginRight: 10 }}>Comprar</Link>
           <Link to="/vender" style={{ marginRight: 10 }}>Vender</Link>
           <Link to="/nuevo-vaper" style={{ marginRight: 10 }}>Nuevo Vaper</Link>
-          <Link to="/estadisticas" style={{ marginRight: 10 }}>Estadísticas</Link>
           <Link to="/finanzas">Finanzas</Link>
         </nav>
 
@@ -21,7 +19,6 @@ function App() {
           <Route path="/comprar" element={<Comprar />} />
           <Route path="/vender" element={<Vender />} />
           <Route path="/nuevo-vaper" element={<NuevoVaper />} />
-          <Route path="/estadisticas" element={<Estadisticas />} />
           <Route path="/finanzas" element={<Finanzas />} />
         </Routes>
       </div>
